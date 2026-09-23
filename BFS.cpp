56f246b45827340a369d99c963bd36cd670da841
Navaneeth <navaneethnaidusulagam@gmail.com>
2026-09-23 17:31:41 +0530

    BFS.cpp

---
+++ BFS.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int main() {
+    int vertices, edges;
+    cout << "Enter number of vertices: ";
+    cin >> vertices;
+
+    cout << "Enter number of edges: ";
+    cin >> edges;
+
+    vector<vector<int>> adj(vertices + 1);
+
+    cout << "Enter edges (u v):" << endl;
+    for (int i = 0; i < edges; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); 
+    }
+
+    int start;
+    cout << "Enter starting vertex for BFS: ";
+    cin >> start;
+
+    vector<bool> visited(vertices + 1, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    cout << "BFS traversal: ";
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
